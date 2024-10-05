2f6e5eaf51a51a4d47aef7f020ba7e9f81671e55
Kaushik Mahadevan <kaushik.mahadevan@gmail.com>
2024-10-05 00:04:40 -0400

    revert

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -21,7 +21,6 @@ export default async function AppHome() {
 
 async function CrudShowcase() {
   const session = await getServerAuthSession();
-  return null;
   if (!session?.user) return null;
 
   const latestPost = await api.post.getLatest.query();
@@ -31,7 +30,7 @@ async function CrudShowcase() {
       <div className="m-2 w-full max-w-xs">
         <div className="flex flex-col items-center justify-center">
           {latestPost ? (
-            <p className="truncate">Your most recent post: latestPost.name</p>
+            <p className="truncate">Your most recent post: {latestPost.name}</p>
           ) : (
             <p>You have no posts yet.</p>
           )}
